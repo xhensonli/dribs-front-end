71d595f5bd07842455534b74d7fea4e644c22505
李宏鑫 <1224435689@qq.com>
2020-05-23 19:47:20 +0800

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button type="primary" disabled>el-button</el-button>
+      <el-button :plain="true" @click="open2">成功</el-button>
+      <el-button :plain="true" @click="open3">警告</el-button>
+      <el-button :plain="true" @click="open1">消息</el-button>
+      <el-button :plain="true" @click="open4">错误</el-button>
+      <el-date-picker
+              v-model="value1"
+              type="date"
+              placeholder="选择日期">
+      </el-date-picker>
     </div>
-    <router-view/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style lang="less">
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      value1: ''
+    }
+  },
+  methods: {
+    open1() {
+      this.$message('这是一条消息提示');
+    },
+    open2() {
+      this.$message({
+        message: '恭喜你，这是一条成功消息',
+        type: 'success'
+      });
+    },
+
+    open3() {
+      this.$message({
+        message: '警告哦，这是一条警告消息',
+        type: 'warning'
+      });
+    },
+
+    open4() {
+      this.$message.error('错了哦，这是一条错误消息');
+    }
+  }
+}
+</script>
+
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
